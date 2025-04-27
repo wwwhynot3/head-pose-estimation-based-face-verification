9d265896611d868dd6ffa2e906accb34a9308d98
Huang Lingbo <wwwhynot3@foxmail.com>
2025-04-27 10:53:08 +0800

    1

---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'edu.wwwhynot3.client',
+  appName: 'client',
+  webDir: 'dist'
+};
+
+export default config;
