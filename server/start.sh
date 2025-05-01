945f09a6fb5d6af94296150a7adcfada469f8b0e
wwwhynot3 <wwwhynot3@foxmail.com>
2025-05-01 18:02:00 +0800

    添加Server启动脚步

--- server/start.sh
+++ server/start.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-sudo ./.venv/bin/uvicorn server.asgi:application --host 0.0.0.0 --port 8000 --ssl-certfile '/home/wwwhynot3/192.168.31.192.pem' --s
-sl-keyfile '/home/wwwhynot3/192.168.31.192-key.pem'
+sudo /home/wwwhynot3/workspace/head-pose-estimation-based-face-verification/server/.venv/bin/uvicorn server.asgi:application --host 0.0.0.0 --port 8000 --ssl-certfile '/home/wwwhynot3/192.168.31.192.pem' --ssl-keyfile '/home/wwwhynot3/192.168.31.192-key.pem'
