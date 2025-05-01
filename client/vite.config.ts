60336b93d0834e7197306157cef0a716dc675c5d
wwwhynot3 <wwwhynot3@foxmail.com>
2025-05-01 17:59:09 +0800

    添加前端跨域支持

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,23 +1,32 @@
 /// <reference types="vitest" />
 
-import legacy from '@vitejs/plugin-legacy'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import { defineConfig } from 'vite'
+import legacy from "@vitejs/plugin-legacy";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { defineConfig } from "vite";
+import fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    legacy()
-  ],
+  plugins: [vue(), legacy()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   test: {
     globals: true,
-    environment: 'jsdom'
-  }
-})
+    environment: "jsdom",
+  },
+  server: {
+    https: {
+      key: fs.readFileSync("/home/wwwhynot3/192.168.31.192-key.pem"),
+      cert: fs.readFileSync("/home/wwwhynot3/192.168.31.192.pem"),
+    },
+    proxy: {
+      '/account': {
+        target: 'http'
+      }
+    }
+  },
+});
